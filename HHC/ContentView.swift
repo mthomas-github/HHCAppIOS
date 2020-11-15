5cd0e4810c2532ce6a2f9b2fb3f8bcfe59ae2371
Michael Thomas <mthomas@easyitconsulting.com>
2020-11-15 11:49:33 -0800

    first commit

---
+++ HHC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HHC
+//
+//  Created by Michael Thomas on 11/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HHC/HHCApp.swift
@@ -0,0 +1,17 @@
+//
+//  HHCApp.swift
+//  HHC
+//
+//  Created by Michael Thomas on 11/15/20.
+//
+
+import SwiftUI
+
+@main
+struct HHCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
